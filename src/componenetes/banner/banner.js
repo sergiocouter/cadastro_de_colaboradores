ce5da815214745dabecf05c0dcb5b15e3566ccb9
sergiocouter <sergiocouter@gmail.com>
2023-04-21 14:59:48 -0300

    Inserindo o banner da página

---
+++ src/componenetes/banner/banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+function Banner(){
+    return(
+        <reader className="banner">
+            <img src="./imagens/banner.png" alt="Banner do site"/>
+        </reader>
+    )
+}
+
+export default Banner;
