343423fec01b5202f7771ab693c638ef2e3b39b3
sergiocouter <sergiocouter@gmail.com>
2023-04-22 20:07:36 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import CampoTexto from './componenetes/banner/CampoTexto/CampoTexto';
 import Banner from './componenetes/banner/banner';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
+      
     </div>
   );
 }
